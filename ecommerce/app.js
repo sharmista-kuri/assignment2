ead0fd34b4ebaa6d0667b33e4455fff7f576639e
sharmista-kuri <skuri.cse@gmail.com>
2024-11-02 23:02:34 -0400

    change in app.js

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -13,53 +13,56 @@ var wishlistRouter = require('./routes/wishlist');
 
 var app = express();
 
+// Enable CORS
 app.use(cors());
 
+// Connect to the database
 const connectDB = require('./db');
 connectDB();
 
-
-
-// view engine setup
+// View engine setup (if you are using templates)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
+// Define routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-//app.use('/api/products', productsRouter);
-
-// app.use('/api/products', (req, res, next) => {
-//   console.log('Request received at /api/products route');
-//   next();
-// }, productsRouter);
-
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/wishlist', wishlistRouter);
 
+// Wildcard route for React client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set the port and start listening
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
